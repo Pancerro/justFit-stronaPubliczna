433dc98e0119786151100871a311da766455c11a
pance <pancerro@gmail.com>
2020-04-01 21:22:39 +0200

    generated deplyables

--- src/app/dashboard/employees/employees.component.ts
+++ src/app/dashboard/employees/employees.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 export class Worker {
+  id:number;
   photo:string;
   name:string;
   surname:string;
   description:string;
   
-  constructor(photo: string,name: string,surname: string,description: string) {
+  constructor(id:number,photo: string,name: string,surname: string,description: string) {
+    this.id=id;
     this.photo = photo;
     this.name= name;
     this.surname=surname;
@@ -22,11 +24,21 @@ export class Worker {
 export class EmployeesComponent implements OnInit {
   constructor(private titleService:Title) { }
   worker:Worker;
-  workers:Worker[]=[new Worker("assets/workers/women.png","Tyska ","Tyskowicz","he he silownia he he"),new Worker("assets/workers/men.png","Dj Bocian","Lazlo","Siłownia to moja pasja")];
+  workers:Worker[]=[new Worker(0,"assets/workers/women.png","Tyska ","Tyskowicz","he he silownia he he"),new Worker(1,"assets/workers/men.png","Dj Bocian","Lazlo","Siłownia to moja pasja")];
   ngOnInit() {
     this.titleService.setTitle("JF_Pracownicy");
     this.worker=this.workers[0];
     
   }
+  nextWorker(id:number){
+    if(id+1==this.workers.length) id=0;
+    else id++;
+    this.worker=this.workers[id]
 
+  }
+  previousWorker(id:number){
+    if(id==0) id=this.workers.length-1;
+    else id--;
+    this.worker=this.workers[id];
+  }
 }
