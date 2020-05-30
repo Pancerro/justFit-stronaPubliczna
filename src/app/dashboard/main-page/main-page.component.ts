c48b2a6d5ae8cf7c0b02f4e90636bfce4b9544ce
Pancerro <pancerro@gmail.com>
2020-05-31 01:31:08 +0200

    update css file

--- src/app/dashboard/main-page/main-page.component.ts
+++ src/app/dashboard/main-page/main-page.component.ts
@@ -14,7 +14,7 @@ export class MainPageComponent implements OnInit {
   onWindowScroll() {
     const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 
-    if (scrollOffset >= 80) {
+    if (scrollOffset >= 40) {
       document.querySelectorAll('.sticky').forEach((c) => {
         c.classList.add('scroll');
       });
